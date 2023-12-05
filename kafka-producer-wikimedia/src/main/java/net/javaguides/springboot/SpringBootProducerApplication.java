aead1e534a761d60060e46aa59623659d6f8e654
Umer <umer.tariq.shaik@softrams.com>
2023-12-05 14:20:47 -0500

    first commit

---
+++ kafka-producer-wikimedia/src/main/java/net/javaguides/springboot/SpringBootProducerApplication.java
@@ -0,0 +1,21 @@
+package net.javaguides.springboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProducerApplication implements CommandLineRunner {
+    public static void main(String[] args){
+        SpringApplication.run(SpringBootProducerApplication.class);
+    }
+    @Autowired
+    private WikimediaChangesProducer wikimediaChangesProducer;
+
+    //runs wheenver applicaiton is started
+    @Override
+    public void run(String... args) throws Exception {
+        wikimediaChangesProducer.sendMesage();
+    }
+}
